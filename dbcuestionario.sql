561a99188d42eee92706882b5d032dacda7b03cd
fret k <146299026+fret3k@users.noreply.github.com>
2024-08-25 20:00:40 -0500

    cod

--- dbcuestionario.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `dbquestionario`. (`id_user` VARCHAR(64) NOT NULL ,
-                                `user` VARCHAR(32) NOT NULL , 
-                                `contrasenia` VARCHAR(32) NOT NULL
-                                , `nombre` VARCHAR(32) NOT NULL , 
-                                `apellidos` VARCHAR(32) NOT NULL , 
-                                `rol` ENUM('administrador','usuario')
-                                NOT NULL , PRIMARY KEY (`id_user`(64)))
-                                ENGINE = InnoDB;
